111d5a4948dc6fcb5df493ef31382a92534be057
Eniola Oladejo <enioladejo1725@gmail.com>
2025-04-07 18:33:44 +0100

    corrected errors

--- src/pages/signin/Signup.jsx
+++ src/pages/signin/Signup.jsx
@@ -1,8 +1,18 @@
-import React from "react";
-import { VStack, Input, Box, Heading, Button, Center, Text, Flex, Field } from "@chakra-ui/react";
-import { PasswordInput } from "../../components/ui/password-input";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import {
+  VStack,
+  Input,
+  Box,
+  Heading,
+  Button,
+  Center,
+  Text,
+  Flex,
+  Field,
+} from '@chakra-ui/react';
+import { PasswordInput } from '../../components/ui/password-input';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
   const {
@@ -16,98 +26,120 @@ export default function Signup() {
     console.log(data);
   };
 
-  const password = watch("password"); // Watch the password field for validation
+  const password = watch('password'); // Watch the password field for validation
 
   const navigate = useNavigate();
 
   const navigateToHome = () => {
-    navigate("/main");
+    navigate('/main');
   };
 
   const navigateToLogin = () => {
-    navigate("/login");
+    navigate('/login');
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Box bg="black" height="100vh">
-        <Heading color={"white"} textAlign="center" pt={5}>
+      <Box bg='black' height='100vh'>
+        <Heading color={'white'} textAlign='center' pt={5}>
           Signup
         </Heading>
         <Box
-          bg={"white"}
+          bg={'white'}
           px={10}
           borderTopLeftRadius={50}
-          overflow={"hidden"}
-          height="100vh"
+          overflow={'hidden'}
+          height='100vh'
           mt={15}
         >
-          <VStack gap="7" width="full" mt={20}>
+          <VStack gap='7' width='full' mt={20}>
             <Field.Root required invalid={!!errors.email}>
-              <Field.Label>
+              <Field.Label color={'black'}>
                 Email <Field.RequiredIndicator />
               </Field.Label>
               <Input
-                {...register("email", { required: "Email is required" })}
-                placeholder="me@example.com"
-                variant="flushed"
+                {...register('email', { required: 'Email is required' })}
+                placeholder='me@example.com'
+                variant='flushed'
+                autoComplete='off'
+                color='gray.800'
               />
               {errors.email && (
                 <Field.ErrorText>{errors.email.message}</Field.ErrorText>
               )}
             </Field.Root>
             <Field.Root required invalid={!!errors.password}>
-              <Field.Label>
+              <Field.Label color={'black'}>
                 Password <Field.RequiredIndicator />
               </Field.Label>
               <PasswordInput
-                {...register("password", { required: "Password is required" })}
-                placeholder="enter your password"
-                variant="flushed"
+                {...register('password', { required: 'Password is required' })}
+                placeholder='enter your password'
+                variant='flushed'
+                autoComplete='off'
+                color='gray.800'
               />
               {errors.password && (
                 <Field.ErrorText>{errors.password.message}</Field.ErrorText>
               )}
             </Field.Root>
             <Field.Root required invalid={!!errors.confirmPassword}>
-              <Field.Label>
+              <Field.Label color={'black'}>
                 Confirm Password <Field.RequiredIndicator />
               </Field.Label>
               <PasswordInput
-                {...register("confirmPassword", {
-                  required: "Confirm Password is required",
+                {...register('confirmPassword', {
+                  required: 'Confirm Password is required',
                   validate: (value) =>
-                    value === password || "Passwords do not match",
+                    value === password || 'Passwords do not match',
                 })}
-                placeholder="confirm your password"
-                variant="flushed"
+                placeholder='confirm your password'
+                variant='flushed'
+                autoComplete='off'
+                color='gray.800'
               />
               {errors.confirmPassword && (
-                <Field.ErrorText>{errors.confirmPassword.message}</Field.ErrorText>
+                <Field.ErrorText>
+                  {errors.confirmPassword.message}
+                </Field.ErrorText>
               )}
             </Field.Root>
             <Field.Root>
-              <Field.Label>Referral</Field.Label>
+              <Field.Label color={'black'}>Referral</Field.Label>
               <Input
-                {...register("referral")}
-                placeholder="enter referral code"
-                variant="flushed"
+                {...register('referral')}
+                placeholder='enter referral code'
+                variant='flushed'
+                autoComplete='off'
+                color='gray.800'
               />
             </Field.Root>
           </VStack>
 
           <Center>
-            <Button w={"50%"} p={5} mt={10} type="submit" onClick={navigateToHome} cursor="pointer">
+            <Button
+              w={'50%'}
+              p={5}
+              mt={10}
+              type='submit'
+              onClick={navigateToHome}
+              cursor='pointer'
+            >
               Submit
             </Button>
           </Center>
           <Flex
-            flexDirection={"row"}
-            alignItems={"center"}
-            justifyContent={"center"}
+            flexDirection={'row'}
+            alignItems={'center'}
+            justifyContent={'center'}
             mt={10}
           >
-            <Text>Already have an account,</Text>
-            <Text color={"blue.500"} ml={1} >
+            <Text color={'black'}>Already have an account?</Text>
+            <Text
+              color={'blue.500'}
+              textDecoration={'underline'}
+              ml={1}
+              _hover={{ cursor: 'pointer' }}
+            >
               Sign in
             </Text>
           </Flex>
